179cbe4cf2d3481782403c54a3c5acf97ff9641a
1shyamp9 <1shyamp9@gmail.com>
2023-07-18 22:22:05 +0530

    first commit

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 export const  app = express();
+configDotenv({
+    path: './database/config.env'
+})
+
 app.use(express.json())
 app.use(cookieParser())
-
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
     methods:["GET","POST","PUT","DELETE"],
@@ -15,7 +18,3 @@ app.use(cors({
 }))
 
 app.use('/user',userRouter)
-
-configDotenv({
-    path: './database/config.env'
-})
