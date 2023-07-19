de3a445e5271cce7c7b7758cc955a0dda859adff
1shyamp9 <1shyamp9@gmail.com>
2023-07-19 11:53:08 +0530

    Error Handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
+import ErrorHandler from "./error.js";
 
 export const isAuth = async(req , res , next)=>{
     try {
         const {token} = req.cookies;
-        if(!token){
-            return res.status(404).json({
-                success:false,
-                message:"Please Login First",
-            })
-        }
+        // if(!token){
+        //     return res.status(404).json({
+        //         success:false,
+        //         message:"Please Login First",
+        //     })
+        // }
+        if(!token) return next(new ErrorHandler("Please Login First",404))
         const decoded = await jwt.verify(token,process.env.JWT_SECRATE);
         req.user = await User.findById(decoded);
         next();
