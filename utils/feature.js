6d02b99e6694c56ce4cec14b53c6d0d6bffc7b22
1shyamp9 <1shyamp9@gmail.com>
2023-07-19 18:23:24 +0530

    Fixed Set Logout time

--- utils/feature.js
+++ utils/feature.js
@@ -5,7 +5,7 @@ export const createCookie = async (res, statusCode, user, message) => {
         const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRATE);
         res.status(statusCode).cookie('token', token, {
             httpOnly: true,
-            maxAge: 15 * 60 * 1000,
+            maxAge: 5 * 60 * 1000,
             sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
             secure: process.env.NODE_ENV === "Development" ? false : true,
         }).json({
